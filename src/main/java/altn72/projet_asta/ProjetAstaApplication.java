9f61ff32cdeb59713502e6acbcf3da3e63742bc5
Rafael53 <91459171+Rafael-oliveira-ferreira@users.noreply.github.com>
2025-10-14 14:14:57 +0200

    init project spring boot

---
+++ src/main/java/altn72/projet_asta/ProjetAstaApplication.java
@@ -0,0 +1,13 @@
+package altn72.projet_asta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAstaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/altn72/projet_asta/ProjetAstaApplicationTests.java
@@ -0,0 +1,13 @@
+package altn72.projet_asta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
