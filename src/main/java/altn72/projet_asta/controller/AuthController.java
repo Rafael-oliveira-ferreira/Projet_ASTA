bcdecb2d107a593f7bebcf99dc6f107419345c9a
Elody <elody.octavie@efrei.net>
2025-10-19 19:19:58 +0200

    added endpoint and login page

---
+++ src/main/java/altn72/projet_asta/controller/AuthController.java
@@ -0,0 +1,14 @@
+package altn72.projet_asta.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "connexion";
+    }
+}
